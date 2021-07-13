b3d37d9665e28aa7c28ab64c9798a01938210185
pznkv <57153340+pznkv@users.noreply.github.com>
2021-07-13 14:11:05 +0300

    added logging

--- data.py
+++ data.py
@@ -1,4 +1,14 @@
 import requests
+import logging
+if __name__ == '__main__':
+    log = logging.getLogger()
+    log.level = logging.DEBUG
+    log.addHandler(logging.StreamHandler(sys.stderr))
+else:
+    log = logging.getLogger(__name__)
+
 a = requests.get('https://api.github.com/events')
 b = requests.post('https://httpbin.org/post', data = {'key':'value'})  
+log.info(a)
+log.info(b)
 
